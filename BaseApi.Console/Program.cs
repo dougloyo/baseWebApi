907cafb576d92132c92a75b96818ca0151857cf4
Douglas Loyo <douglasloyo@gmail.com>
2017-09-24 08:33:00 -0500

    Adding Console application to do OWIN self hosted.

---
+++ BaseApi.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using BaseApi.Web;
+using Microsoft.Owin.Hosting;
+
+namespace BaseApi.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/test").Result;
+
+                System.Console.WriteLine(response);
+                System.Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                System.Console.ReadLine();
+            }
+        }
+    }
+}
