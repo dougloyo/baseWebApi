0ac45c53e5752c6a229c59610a24ea15e83794d9
Douglas Loyo <douglasloyo@gmail.com>
2017-09-24 08:07:52 -0500

    Adding base WebApi and Owin Startup.

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace BaseApi.Web.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes enabled as attributes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+               name: "DefaultApi",
+               routeTemplate: "api/{controller}/{id}",
+               defaults: new { id = RouteParameter.Optional }
+           );
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using BaseApi.Web.App_Start;
+using Owin;
+
+namespace BaseApi.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var httpConfiguration = new HttpConfiguration();
+            WebApiConfig.Register(httpConfiguration);
+            appBuilder.UseWebApi(httpConfiguration);
+        }
+    }
+}
